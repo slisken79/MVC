39d2ec568d241101cf87d3719f541d1af92f509e
slisken79 <essadak@yahoo.se>
2016-04-19 13:12:33 +0200

    Lab1MVC

---
+++ MVCLab1/MVCLab1/Controllers/LifeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCLab1.Controllers
+{
+    public class LifeController : Controller
+    {
+        // GET: Life
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult SuperMario()
+        {
+            return View();
+        }
+        public ActionResult Health()
+        {
+            return View();
+        }
+        public ActionResult Live()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVCLab1/MVCLab1/Controllers/SellController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCLab1.Controllers
+{
+    public class SellController : Controller
+    {
+        //Upgifft 4: Det saknades en index action och en view på SellController
+        // GET: Sell
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult C2C()
+        {
+            return View();
+        }
+        public ActionResult Completion()
+        {
+            return View();
+        }
+        public ActionResult B2B()
+        {
+            return View();
+        }
+        public ActionResult B2C()
+        {
+            return View();
+        }
+    }
+}
