28e268b7b1a7fa7dc0e5c587168f50ea6e643e3b
slisken79 <essadak@yahoo.se>
2016-05-04 09:17:07 +0200

    Validation

--- Lab3/MvcMovie/Migrations/20160504070749_DataAnnotations.Designer.cs
+++ Lab3/MvcMovie/Migrations/20160504070749_DataAnnotations.Designer.cs
@@ -8,9 +8,10 @@ using MvcMovie.Models;
 namespace MvcMovie.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20160504070749_DataAnnotations")]
+    partial class DataAnnotations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "7.0.0-rc1-16348")
