6dd38feb5992c25fedaac430866067e3963fbae8
slisken79 <essadak@yahoo.se>
2016-05-06 03:43:09 +0200

    Lab2

---
+++ Lab2/TicTacToe/Controllers/TicTacToeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TicTacToe.Models;
+
+namespace TicTacToe.Controllers
+{
+    public class TicTacToeController : Controller
+    {
+        // GET: TicTacToe
+        public ActionResult Index()
+        {
+            var ticModel = new TicTacToeModel();
+            this.Session["Tic"] = ticModel;
+            return View(ticModel);
+
+        }
+        //public ActionResult Reset()
+        //{
+        //    var ticModel = new TicTacToeModel();
+        //    this.Session["Tic"] = ticModel;
+        //    return View(ticModel);
+
+        //}
+        [HttpPost]
+        public ActionResult Index(int? choice)
+        {
+            if (choice == 10)
+            {
+                var ticModel2 = new TicTacToeModel();
+                this.Session["Tic"] = ticModel2;
+                return View(ticModel2);
+            }         
+            var ticModel = (TicTacToeModel) this.Session["Tic"];
+            ticModel.Human(choice.Value);
+           
+            return View(ticModel);
+            
+        }
+    }
+}
---
+++ Lab2/TicTacToe/Models/Cell.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Web;
+
+namespace TicTacToe.Models
+{
+    public class Cell
+    {
+        
+
+    }
+}
