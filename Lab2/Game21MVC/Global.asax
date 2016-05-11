254c7fb10acf8a63cbff3d2e0abda8c434d20643
slisken79 <essadak@yahoo.se>
2016-05-12 00:15:49 +0200

    yes

---
+++ Lab2/Game21MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Game21MVC.MvcApplication" Language="C#" %>
---
+++ Lab2/Game21MVC/Models/GameOf21.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Game21MVC.Models
+{
+    public class GameOf21
+    {
+        public int Total { get; set; } = 0;
+        public string Announcement { get; set; }
+
+        public void AddNr(int tal)
+        {
+            if (Total <= 21)
+            {
+                Total += tal;
+                if (Total == 21)
+                {
+                    Announcement = "You Win!!";
+                }
+            }
+
+            Computer();
+        }
+
+        private void Computer()
+        {
+            if (Total >= 12)
+            {
+                Total += (Total + 1) % 3 == 0 ? 1 : 2;
+                if (Total == 21)
+                {
+                    Announcement = "Computer Won!!";
+                }
+            }
+
+            if (Total < 12)
+            {
+                var random = new Random();
+                Total += random.Next(1, 3);
+            }
+        }
+    }
+}
